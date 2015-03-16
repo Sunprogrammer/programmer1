703da36f817d98c6dd68e6b48ab90b724978616a
Sunprogrammer <carlos_sk8ever_@hotmail.com>
2015-03-16 15:53:26 -0300

    Create Main.h

---
+++ Client-System-one/Main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <windows.h>
+#include  <stdio.h>
+#include  <commctrl.h>
+#include <iostream>
+#pragma comment(lib, "comctl32.lib")
+#include <CString.h>
+
+#include "Utils.h"
